4e37d27b2ec6b577b23c24e7277833b6fd835e50
Manish <sggaming705@gmail.com>
2026-03-01 17:56:37 +0530

    commit backend

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -368,4 +368,3 @@ resource "aws_iam_role_policy_attachment" "lambda_sns_policy_attachment" {
   role       = aws_iam_role.lambda_exec_role.name
   policy_arn = aws_iam_policy.devops_accelerator_lambda_sns_policy.arn
 }
-#manish
