fe164b3762b36c4bf1f7d25521b680485d619f50
Manish <sggaming705@gmail.com>
2026-03-01 16:28:36 +0530

    Fix IAM Duplicate using name_prfix

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -231,7 +231,7 @@ resource "aws_iam_role" "presign_lambda_role" {
 }
 
 resource "aws_iam_policy" "presign_lambda_policy" {
-  name = "DevOps-Accelerator-Presign-Lambda-Policy"
+  name_prefix = "DevOps-Accelerator-Presign-Lambda-Policy"
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -350,7 +350,7 @@ resource "aws_sns_topic_subscription" "devops_accelerator_email_sub" {
 }
 
 resource "aws_iam_policy" "devops_accelerator_lambda_sns_policy" {
-  name = "devops-accelerator-lambda-sns-publish-policy"
+  name_prefix = "devops-accelerator-lambda-sns-publish-policy"
 
   policy = jsonencode({
     Version = "2012-10-17",
@@ -368,4 +368,3 @@ resource "aws_iam_role_policy_attachment" "lambda_sns_policy_attachment" {
   role       = aws_iam_role.lambda_exec_role.name
   policy_arn = aws_iam_policy.devops_accelerator_lambda_sns_policy.arn
 }
-   ##
